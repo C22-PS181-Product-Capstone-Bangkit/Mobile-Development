cf2d0104e26a2084f717c30cd8d9598a334e6880
Richard Tanoto <richard.tanoto@ui.ac.id>
2022-05-17 15:16:36 +0700

    Revisi Landing Screen (Final)

---
+++ app/src/main/java/com/bangkit/cemil/LandingActivity.kt
@@ -0,0 +1,65 @@
+package com.bangkit.cemil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.bangkit.cemil.databinding.ActivityLandingBinding
+import com.google.android.material.tabs.TabLayoutMediator
+
+class LandingActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLandingBinding
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        binding = ActivityLandingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        val viewPagerAdapter = LandingScreenAdapter(this)
+        binding.landingViewPager.adapter = viewPagerAdapter
+
+        TabLayoutMediator(binding.landingTabLayout, binding.landingViewPager) { _, _ ->
+            // Do nothing
+        }.attach()
+
+        binding.landingNextButton.setOnClickListener {
+            binding.landingViewPager.currentItem++
+        }
+
+        binding.landingGetStartedButton.setOnClickListener {
+            // Navigate to Home Screen
+        }
+
+        binding.landingViewPager.registerOnPageChangeCallback(object:
+            ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                isTheEndOfLandingScreen(position)
+            }
+        })
+    }
+
+    private fun isTheEndOfLandingScreen(currentItem: Int) {
+        if (currentItem == 2) {
+            binding.landingNextButton.visibility = View.INVISIBLE
+            binding.landingGetStartedButton.visibility = View.VISIBLE
+        } else {
+            binding.landingGetStartedButton.visibility = View.INVISIBLE
+            binding.landingNextButton.visibility = View.VISIBLE
+        }
+    }
+
+    private inner class LandingScreenAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity){
+        override fun getItemCount(): Int = 3
+
+        override fun createFragment(position: Int): Fragment {
+            return LandingFragment.getInstance(position)
+        }
+    }
+}
