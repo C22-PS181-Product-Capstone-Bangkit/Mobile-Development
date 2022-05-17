50bea87650c285cda17218319a1b994b725bc53a
rainzoneg <samuelnathaniel1234@gmail.com>
2022-05-17 12:15:22 +0700

    Register Success fragment basic layout

--- app/src/main/java/com/bangkit/cemil/authentication/RegisterFragment.kt
+++ app/src/main/java/com/bangkit/cemil/authentication/RegisterFragment.kt
@@ -1,20 +1,21 @@
-package com.bangkit.cemil
+package com.bangkit.cemil.authentication
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bangkit.cemil.R
 
 
-class LoginFragment : Fragment() {
+class RegisterFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_register, container, false)
     }
 
 }
