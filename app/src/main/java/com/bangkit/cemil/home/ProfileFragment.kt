6a2087f590ff5b08d60bf865b11228fbf8f2a389
rainzoneg <samuelnathaniel1234@gmail.com>
2022-05-21 12:13:51 +0700

    Added profile layout and testing navUI functionality for the history row

--- app/src/main/java/com/bangkit/cemil/home/ProfileFragment.kt
+++ app/src/main/java/com/bangkit/cemil/home/ProfileFragment.kt
@@ -5,56 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.bangkit.cemil.R
+import com.bangkit.cemil.databinding.FragmentProfileBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding : FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container ,false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val bottomNavigation : BottomNavigationView = requireActivity().findViewById(R.id.bottomNavigationView)
+        binding.menuHistory.setOnClickListener {
+            NavigationUI.onNavDestinationSelected(bottomNavigation.menu.findItem(R.id.historyFragment),it.findNavController())
+        }
+        binding.menuReviews.setOnClickListener {
+
+        }
+        binding.menuRecommendations.setOnClickListener {
+
+        }
+        binding.menuAppearances.setOnClickListener {
+
+        }
+        binding.menuLanguage.setOnClickListener {
+
+        }
+        binding.menuAbout.setOnClickListener {
+
+        }
+        binding.menuChangePass.setOnClickListener {
+
+        }
+        binding.menuLogout.setOnClickListener {
+
+        }
     }
 }
